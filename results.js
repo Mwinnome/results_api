4d489f186e014d484c0fef7d558d3ab5d57f58db
Mwinnome <101268966+Mwinnome@users.noreply.github.com>
2022-04-06 07:41:10 -0700

    first commit

---
+++ results.js
@@ -0,0 +1,14 @@
+const results =[
+{name:"Windy Dari", class:"Form One", marks:8},
+{name:"Mercy Jackson", class:"Form One", marks:8},
+{name:"Mwinnome Pee", class:"Form One", marks:9},
+{name:"Cinderilla Naa", class:"Form One", marks:7},
+{name:"Windy Shay", class:"Form One", marks:7},
+{name:"Windy Dari", class:"Form One", marks:8},
+{name:"Mercy Jackson", class:"Form One", marks:8},
+{name:"Mwinnome Pee", class:"Form One", marks:9},
+{name:"Cinderilla Naa", class:"Form One", marks:7},
+{name:"Windy Shay", class:"Form One", marks:7},
+]
+
+module.exports=results
